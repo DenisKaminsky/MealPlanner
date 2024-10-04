659f7733212490eaa6fe3ff6ee5d4517618bdb9b
Denis Kaminsky <kkaminsky.denis@gmail.com>
2024-10-04 12:54:59 +0200

    Added init script

---
+++ MealPlanner.Data/Scripts/init.sh
@@ -0,0 +1,9 @@
+﻿use MealPlannerDb
+
+db.createCollection("ProductCategory");
+db.ProductCategory.createIndex({ "Name": 1 });
+
+
+db.createCollection("Product");
+db.Product.createIndex({ "Name": 1 });
+db.Product.createIndex({ "CategoryId": 1 });
